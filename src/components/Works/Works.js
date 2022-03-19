69f8f150a60281316020afda1934f903824b088a
Roman Chernik <chernik.r@yandex.ru>
2022-03-19 21:47:54 +0300

    feat: добавить изображение 3го сайта

--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -1,6 +1,7 @@
 import './Works.css';
 import howToLearnImage from '../../images/works/1.png';
 import russianTravelImage from '../../images/works/2.png';
+import mestoImage from '../../images/works/3.png'
 
 export default function Works() {
     return (
@@ -25,7 +26,7 @@ export default function Works() {
                 <article className="works__card">
                     <img className="works__front-side" src={russianTravelImage} alt="Russian travel" />
                     <div className="works__back-side">
-                    <h3 className="works__back-side-title">How to learn</h3>
+                        <h3 className="works__back-side-title">How to learn</h3>
                         <p className="works__back-side-description">
                             Landing page. Адаптивная верстка для разных устройств отсутствует. Расположения элементов
                             выполнено с помощью позиционирования и flexbox. Структура документа соответствует семантике.
@@ -38,9 +39,9 @@ export default function Works() {
                     </div>
                 </article>
                 <article className="works__card">
-                    <img className="works__front-side" src={howToLearnImage} alt="How to rearn" /> 
+                    <img className="works__front-side" src={mestoImage} alt="Mesto" /> 
                     <div className="works__back-side">
-                    <h3 className="works__back-side-title">How to learn</h3>
+                        <h3 className="works__back-side-title">Mesto</h3>
                         <p className="works__back-side-description">
                             Landing page. Адаптивная верстка для разных устройств отсутствует. Расположения элементов
                             выполнено с помощью позиционирования и flexbox. Структура документа соответствует семантике.
@@ -53,9 +54,9 @@ export default function Works() {
                     </div>
                 </article>
                 <article className="works__card">
-                    <img className="works__front-side" src={howToLearnImage} alt="How to rearn" />
+                    <img className="works__front-side" src={mestoImage} alt="How to rearn" />
                     <div className="works__back-side">
-                    <h3 className="works__back-side-title">How to learn</h3>
+                        <h3 className="works__back-side-title">How to learn</h3>
                         <p className="works__back-side-description">
                             Landing page. Адаптивная верстка для разных устройств отсутствует. Расположения элементов
                             выполнено с помощью позиционирования и flexbox. Структура документа соответствует семантике.
