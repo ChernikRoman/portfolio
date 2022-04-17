29a4aabe7cfc7921809cc9eb3a33ebd35b0918d4
Roman Chernik <chernik.r@yandex.ru>
2022-04-18 01:34:27 +0300

    refactor: отредактировать описание about-me

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -5,20 +5,6 @@ export default function Contacts() {
         <>
             <p>Со мной можно связаться:</p>
             <address className="contacts" style={{textAlign: "center"}}>
-                {/* <ul>
-                    <li>
-                        Написать на почту: <a href="mailto:chernik.r@yandex.ru">chernik.r@yandex.ru</a>
-                    </li>
-                    <li>
-                        Cвязаться по телефону: <a href="tel:+79771811931">+789771811931</a>
-                    </li>
-                    <li>
-                        Написать в телеграм: <a href="https://t.me/chernikroman">@chernikroman</a>
-                    </li>
-                    <li>
-                        Или What`s App: <a href="https://wa.me/+79999109370">+79999109370</a>
-                    </li>
-                </ul> */}
                 Написать на почту: <a href="mailto:chernik.r@yandex.ru">chernik.r@yandex.ru</a>
                 <br />
                 Cвязаться по телефону: <a href="tel:+79771811931">+789771811931</a>
